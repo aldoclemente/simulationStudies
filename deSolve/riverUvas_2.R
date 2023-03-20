5ce92002774345daafc1dd75103859ecf3b10980
aldoclemente <aldo.clemente@polimi.it>
2023-03-20 20:18:44 +0000

    minor changes.

--- deSolve/riverUvas_2.R
+++ deSolve/riverUvas_2.R
@@ -11,7 +11,6 @@ Nb = riverlen/dx # number of interval
                  # Nb + 1 mesh nodes
 xgrid = seq(from=0, to=riverlen, length.out=(Nb+1)) # mesh nodes
 edges = cbind( seq(from=1,to=Nb,by=1), seq(from=2,to=Nb+1,by=1))
-edges
 
 neigh = neigh1D(edges)
 boundary = matrix(0,nrow=(Nb+1), ncol=1)
@@ -128,10 +127,10 @@ rhs_ = forcingVec + 1/dt_*(Mass%*%C_[1:(Nb+1),(k-1)] ) + Mass%*%exchange + ased/
 A_ = (1/dt_ * Mass + Stiff)
 
 C_[1:(Nb+1),k] = as.vector(solve(A_, rhs_))
-C_[(Nb+2):(2*(Nb+1)),k] = C_[(Nb+2):(2*(Nb+1)),(k-1)] - dt_* area/area2*alpha*(C_[(Nb+2):(2*(Nb+1)),(k-1)] - C_[1:(Nb+1),(k)] )            #exchange 
-                                                      + dt_* sorpS * (CsB - C_[1:(Nb+1),(k)]) #* (CsB - C_[1:(Nb+1),(k-1)]) 
+C_[(Nb+2):(2*(Nb+1)),k] = C_[(Nb+2):(2*(Nb+1)),(k-1)] - dt_* area/area2*exchange 
+                                                      + dt_* sorpS* (CsB - C_[(Nb+2):(2*(Nb+1)),(k-1)])  
 
-C_[(2*(Nb+1)+1):(3*(Nb+1)), k] = C_[(2*(Nb+1)+1):(3*(Nb+1)), (k-1)] - dt_* sorp*(C_[(2*(Nb+1)+1):(3*(Nb+1)), (k-1)] - C_[1:(Nb+1),(k)]) # sorption
+C_[(2*(Nb+1)+1):(3*(Nb+1)), k] = C_[(2*(Nb+1)+1):(3*(Nb+1)), (k-1)] - dt_*sorption
 
 }
 
@@ -151,3 +150,133 @@ filled.contour(x=times/3600, y=xgrid, t(C_[(2*(Nb+1)+1):(3*(Nb+1)),]))
 dev.off()
 
 
+
+
+for(k in 2:length(times)){
+
+dirichletBC[1]    = Cfun(times[k])
+dirichletBC[Nb+1] = C_[(Nb+1),k-1]    #C_[(Nb+1),k-1]    # default deSolve (?)
+
+PDE$set_dirichletBC(dirichletBC)      #C_[2*(Nb+1)+1, 3*(Nb+1), (k-1)]
+
+PDE$set_forcingTerm( as.matrix(force_)) 
+res = PDE$solve() # matrices
+Stiff = res$Stiff
+Mass  = res$Mass 
+
+# PDE - Implicit Euler
+exchange = alpha*(C_[(Nb+2):(2*(Nb+1)),(k-1)] - C_[1:(Nb+1),(k-1)] )
+sorption = sorp*(C_[(2*(Nb+1)+1):(3*(Nb+1)), (k-1)] - C_[1:(Nb+1),(k-1)])
+forcingVec = PDE$force()
+
+rhs_ = forcingVec + 1/dt_*(Mass%*%C_[1:(Nb+1),(k-1)] ) + Mass%*%exchange + ased/area* (Mass%*%sorption) 
+A_ = (1/dt_ * Mass + Stiff)
+
+C_[1:(Nb+1),k] = as.vector(solve(A_, rhs_))
+
+exchange = alpha*(C_[(Nb+2):(2*(Nb+1)),(k-1)] - C_[1:(Nb+1),k] )
+sorption = sorp*(C_[(2*(Nb+1)+1):(3*(Nb+1)), (k-1)] - C_[1:(Nb+1),k])
+
+C_[(Nb+2):(2*(Nb+1)),k] = C_[(Nb+2):(2*(Nb+1)),(k-1)] - dt_* area/area2*exchange 
+                                                      + dt_ * sorpS * (CsB - C_[(Nb+2):(2*(Nb+1)),k])  
+
+C_[(2*(Nb+1)+1):(3*(Nb+1)), k] = C_[(2*(Nb+1)+1):(3*(Nb+1)), (k-1)] - dt_*sorption
+
+}
+
+pdf("RiverUvas_backwardEuler_ODE.pdf")
+filled.contour(x=times/3600, y=xgrid, t(C_[1:(Nb+1),]))
+filled.contour(x=times/3600, y=xgrid, t(C_[(Nb+2):(2*(Nb+1)),]))
+filled.contour(x=times/3600, y=xgrid, t(C_[(2*(Nb+1)+1):(3*(Nb+1)),]))
+dev.off()
+
+
+{
+
+library(fdaPDE)
+mesh_aux = create.mesh.1.5D(cbind(River$nodes, rep(0,nrow=(Nb+1))), River$elements)
+
+FEMbasis = create.FEM.basis(mesh_aux)
+
+PDE_parameters <- list("diffusion" = D, 
+                       "transport" = as.matrix(Q/area),
+                        "reaction" = (qlat/area+alpha+ ased/area*sorp))
+dt_ = 120
+
+C_ = matrix(0, nrow=3*(Nb+1),ncol=length(times))
+C_[,1] = Yini
+
+PDE <- new(PDE_1D_isotropic_ORDER_1, River)
+PDE$set_PDEparameters(PDE_parameters)
+quadrature_nodes = PDE$get_quadrature_nodes()
+
+dirichletBC = matrix(0, nrow=Nb+1,ncol=1) 
+
+forcing_ = function(x){
+    return(qlat*Clat/area* x^0)
+}
+
+#### Setting forcing term ####
+force_ = forcing_(quadrature_nodes)
+PDE$set_forcingTerm( as.matrix(force_) ) 
+
+for(k in 2:length(times)){
+
+dirichletBC[1]    = Cfun(times[k])
+dirichletBC[Nb+1] = C_[(Nb+1),k-1]    #C_[(Nb+1),k-1]    # default deSolve (?)
+
+PDE$set_dirichletBC(dirichletBC)      #C_[2*(Nb+1)+1, 3*(Nb+1), (k-1)]
+
+forcing_TMP = as.matrix(force_) 
+              + eval.FEM( FEM(coeff= alpha          *C_[(Nb+2):(2*(Nb+1)),(k-1)],       FEMbasis=FEMbasis), locations=quadrature_nodes )
+              + eval.FEM( FEM(coeff= ased/area*sorp *C_[(2*(Nb+1)+1):(3*(Nb+1)), (k-1)], FEMbasis=FEMbasis), locations=quadrature_nodes )
+PDE$set_forcingTerm(forcing_TMP) 
+res = PDE$solve() # matrices
+Stiff = res$Stiff
+Mass  = res$Mass 
+
+# PDE - Implicit Euler
+forcingVec = PDE$force() 
+
+rhs_ = forcingVec + 1/dt_*(Mass%*%C_[1:(Nb+1),(k-1)] ) #+ alpha*(Mass%*%C_[(Nb+2):(2*(Nb+1)),(k-1)]) 
+                                                       #+ ased/area*sorp * (Mass%*%C_[(2*(Nb+1)+1):(3*(Nb+1)), (k-1)]) 
+A_ = (1/dt_ * Mass + Stiff)
+
+C_[1:(Nb+1),k] = as.vector(solve(A_, rhs_))
+
+#exchange = alpha*(C_[(Nb+2):(2*(Nb+1)),(k-1)] - C_[1:(Nb+1),(k-1)] )
+#sorption = sorp*(C_[(2*(Nb+1)+1):(3*(Nb+1)), (k-1)] - C_[1:(Nb+1),(k-1)])
+C_[(Nb+2):(2*(Nb+1)),k] = (         1/dt_ * C_[(Nb+2):(2*(Nb+1)),(k-1)] 
+                         + alpha*area/area2*C_[(Nb+2):(2*(Nb+1)),k] 
+                         +           sorpS* CsB) / (1/dt_ + alpha*area/area2 + sorpS)  
+
+C_[(2*(Nb+1)+1):(3*(Nb+1)), k] = (1/dt_*C_[(2*(Nb+1)+1):(3*(Nb+1)), (k-1)] + sorp*C_[1:(Nb+1),(k-1)] ) / (1/dt_ + sorp)
+
+}
+
+x11()
+filled.contour(x=times/3600, y=xgrid, t(C_[1:(Nb+1),]))
+
+x11()
+filled.contour(x=times/3600, y=xgrid, t(C_[(Nb+2):(2*(Nb+1)),]))
+
+x11()
+filled.contour(x=times/3600, y=xgrid, t(C_[(2*(Nb+1)+1):(3*(Nb+1)),]))
+
+pdf("RiverUvas_femR_mody.pdf")
+image(x=times/3600, y=xgrid, t(C_[1:(Nb+1),]))
+image(x=times/3600, y=xgrid, t(C_[(Nb+2):(2*(Nb+1)),]))
+image(x=times/3600, y=xgrid, t(C_[(2*(Nb+1)+1):(3*(Nb+1)),]))
+
+x11()
+plot(times/3600, C_[38,], type="l", xlab="time, hour", ylab="Conc", main="Strontium",ylim=range(C_))
+points(times/3600, C_[38,])
+
+points(times/3600, C_[281,], type="l", xlab="time, hour", ylab="Conc", main="Strontium")
+points(times/3600, C_[281,], pch=16)
+legend("topright", legend=c("38m", "281m"), pch=c(1,16))
+
+
+
+
+}
